fe654d1a485f7b04cfa23cdbded16ce512063f07
ahmed <ahmedgh@rosafian.com>
2018-09-22 15:29:06 +0100

    feat(weather): transform current weather

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -8,6 +8,7 @@ import { WeatherService } from '../weather/weather.service';
   styleUrls: ['./current-weather.component.scss']
 })
 export class CurrentWeatherComponent implements OnInit {
+
   current: any = [];
 
   constructor(private weatherService: WeatherService) { }
@@ -17,18 +18,26 @@ export class CurrentWeatherComponent implements OnInit {
     this.weatherService.getCurrentWeather('tunis', 'tn')
       .subscribe((data) => {
         debugger
-        this.current = data
+        this.current = this.transformCurrentWeather(data)
       })
-
-    // mock api
-    // this.current = {
-    //   city: 'Bethesda',
-    //   country: 'US',
-    //   date: new Date(),
-    //   image: 'assets/img/sunny.svg',
-    //   temperature: 72,
-    //   description: 'sunny',
-    // } as ICurrentWeather
   }
+
+
+  transformCurrentWeather(data: any) {
+    return {
+      city: data.name,
+      country: data.sys.country,
+      date: data.dt * 1000,
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
+      temperature: this.convertKelvinToFahrenheit(data.main.temp),
+      description: data.weather[0].description
+    }
+  }
+
+
+  convertKelvinToFahrenheit(kelvin: number): number {
+    return kelvin * 9 / 5 - 459.67;
+  }
+
 
 }
